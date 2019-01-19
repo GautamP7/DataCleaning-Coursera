1fcd86fe1319e97a302e0057d1cb4eb421884495
Gautam <gautam_1716@yahoo.co.in>
2019-01-19 02:44:08 -0600

    Update run_analysis.r

--- run_analysis.R
+++ run_analysis.R
@@ -19,7 +19,7 @@ f_data[[68]] <- key[match(f_data[[68]], key[[1]]), 2]
 
 colnames(f_data) <- c('subjectId', 'tBodyAccMeanX', 'tBodyAccMeanY', 'tBodyAccMeanZ', 'tBodyAccStdX', 'tBodyAccStdY', 'tBodyAccStdZ', 'tGravityAccMeanX', 'tGravityAccMeanY', 'tGravityAccMeanZ', 'tGravityAccStdX', 'tGravityAccStdY', 'tGravityAccStdZ', 'tBodyAccJerkMeanX', 'tBodyAccJerkMeanY', 'tBodyAccJerkMeanZ', 'tBodyAccJerkStdX', 'tBodyAccJerkStdY', 'tBodyAccJerkStdZ', 'tBodyGyroMeanX', 'tBodyGyroMeanY', 'tBodyGyroMeanZ', 'tBodyGyroStdX', 'tBodyGyroStdY', 'tBodyGyroStdZ', 'tBodyGyroJerkMeanX', 'tBodyGyroJerkMeanY', 'tBodyGyroJerkMeanZ', 'tBodyGyroJerkStdX', 'tBodyGyroJerkStdY', 'tBodyGyroJerkStdZ', 'tBodyAccMagMean', 'tBodyAccMagStd', 'tGravityAccMagMean', 'tGravityAccMagStd', 'tBodyAccJerkMagMean', 'tBodyAccJerkMagStd', 'tBodyGyroMagMean', 'tBodyGyroMagStd', 'tBodyGyroJerkMagMean', 'tBodyGyroJerkMagStd', 'fBodyAccMeanX', 'fBodyAccMeanY', 'fBodyAccMeanZ', 'fBodyAccStdX', 'fBodyAccStdY', 'fBodyAccStdZ', 'fBodyAccJerkMeanX', 'fBodyAccJerkMeanY', 'fBodyAccJerkMeanZ', 'fBodyAccJerkStdX', 'fBodyAccJerkStdY', 'fBodyAccJerkStdZ', 'fBodyGyroMeanX', 'fBodyGyroMeanY', 'fBodyGyroMeanZ', 'fBodyGyroStdX', 'fBodyGyroStdY', 'fBodyGyroStdZ', 'fBodyAccMagMean', 'fBodyAccMagStd', 'fBodyBodyAccJerkMagMean', 'fBodyBodyAccJerkMagStd', 'fBodyBodyGyroMagMean', 'fBodyBodyGyroMagStd', 'fBodyBodyGyroJerkMagMean', 'fBodyBodyGyroJerkMagStd', 'activity')
 
-rm('data1_x', 'data1_y', 'data1_s', 'data2_x', 'data2_y', 'data2_s', 'key', 'train', 'test', 'data', 'X')
+rm('data1_x', 'data1_y', 'data1_s', 'data2_x', 'data2_y', 'data2_s', 'key', 'train', 'test', 'data')
 
 library(dplyr)
 subact <- f_data %>%
